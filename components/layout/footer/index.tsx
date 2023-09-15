5ac37cb7da7492655429e224658f132e5396df5d
ShiwenHan <han.1970@osu.edu>
2023-09-15 17:28:17 -0400

    footer, icon

---
+++ components/layout/footer/index.tsx
@@ -0,0 +1,18 @@
+import LocationIcon from "@/components/icons/location";
+
+export default async function Footer() {
+  return (
+    <div className=" bg-black">
+      <div className=" container px-10 py-10 mx-auto flex items-center justify-between">
+        <div className=" inline-flex">
+          <LocationIcon />
+          <span className=" text-white text-xs font-bold px-2">Ohio, US</span>
+          <div className=" text-gray-300 text-xs px-6">
+            © 2023 All Rights Reserved
+          </div>
+        </div>
+        <div className=" text-gray-300 text-xs">CSE5234</div>
+      </div>
+    </div>
+  );
+}
