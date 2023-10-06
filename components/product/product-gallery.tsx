6514bfda931d74d63150d5a7510c568ac6e62c0f
ShiwenHan <han.1970@osu.edu>
2023-10-05 23:15:29 -0400

    fix key

--- components/product/product-gallery.tsx
+++ components/product/product-gallery.tsx
@@ -6,13 +6,11 @@ export default async function ProductGallery() {
   const products: Products = await getAllProducts();
   return (
     <div className=" product-gallery-container gap-y-6 flex flex-wrap ">
-      
       {products.map((product) => (
-        < ProductCard product = {product} />
+        <ProductCard key={product.id} product={product} />
       ))}
 
       {/* < ProductCard product = {products[0]} /> */}
-    
     </div>
   );
 }
