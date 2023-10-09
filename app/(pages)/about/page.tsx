8d7fb467d13abfc448ddee5870e21999e43c8c17
ButtonBill <Willakarra@gmail.com>
2023-10-08 21:15:56 -0400

    About page done, still need a bit of info.

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,7 +1,57 @@
 export default function About() {
   return (
-    <div className="flex flex-col items-center justify-between p-24">
-      hello, about
+    <div className = "about">
+      <div className="body">
+      <div className="developers">
+        <div className="text-wrapper">Developers</div>
+        <div className="frame">
+          <div className="developer">
+            <div className="profile">
+              <img className="image" alt="Image" src="https://avatars.githubusercontent.com/u/46259514?s=64&v=4" />
+              <div className="info">
+                <div className="div">Shiwen Han</div>
+                <div className="text-wrapper-2">han.1970@buckeyemail.osu.edu</div>
+              </div>
+            </div>
+            <div className="text-wrapper-3">CFO, MSCSE at Ohio State University.</div>
+          </div>
+          <div className="developer">
+            <div className="profile">
+              <img className="image" alt="Image" src="https://avatars.githubusercontent.com/u/13644713?v=4" />
+              <div className="info">
+                <div className="div">Will Keiser</div>
+                <div className="text-wrapper-2">keiser.70@buckeyemail.osu.edu</div>
+              </div>
+            </div>
+            <div className="text-wrapper-3">VP of Engineering. 4 years of education at The Ohio State University, and 1 year of experience with J.P. Morgan Chase.</div>
+          </div>
+          <div className="developer">
+            <div className="profile">
+              <img className="image" alt="Image" src="https://avatars.githubusercontent.com/u/91095404?s=96&v=4" />
+              <div className="info">
+                <div className="div">Hailie Payne</div>
+                <div className="text-wrapper-2">email@buckeyemail.osu.edu</div>
+              </div>
+            </div>
+            <div className="text-wrapper-3">Description</div>
+          </div>
+          <div className="developer">
+            <div className="profile">
+              <img className="image" alt="Image" src="INSERT LINK TO IMAGE HERE" />
+              <div className="info">
+                <div className="div">Name</div>
+                <div className="text-wrapper-2">email@buckeyemail.osu.edu</div>
+              </div>
+            </div>
+            <div className="text-wrapper-3">Description</div>
+          </div>
+        </div>
+      </div>
+      <div className="about-us">
+        <div className="text-wrapper">About Us</div>
+        <div className="text-wrapper-4"> An E-Commerce Website designed for CSE 5234. Data used for the project is scraped from Nike Inc, so our website name is Nikia. This website was created using NextJS.</div>
+      </div>
     </div>
+    </div>    
   );
-}
+};
