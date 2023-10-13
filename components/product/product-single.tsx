1254325a14ad66c1e9e0cb5e45533274ce46d2dd
ShiwenHan <han.1970@osu.edu>
2023-10-13 03:11:32 -0400

    product single response

--- components/product/product-single.tsx
+++ components/product/product-single.tsx
@@ -27,8 +27,14 @@ export default function ProductSingle({ product }: { product: Product }) {
     <div className=" container mx-auto">
       <div className=" mx-auto w-fit my-20">
         <div className=" flex flex-col gap-12 lg:gap-20 lg:flex-row">
-          <div className=" flex flex-row gap-3 lg:min-w-[556px]">
-            <div className=" no-scrollbar flex flex-col gap-4 max-h-[450px] overflow-scroll">
+          <div className=" flex flex-col sm:flex-row gap-3 mx-auto w-fit lg:min-w-[556px]">
+            <img
+              className=" w-80 h-fit block sm:hidden"
+              key={product.id}
+              src={selectedImage}
+              alt={product.name}
+            />
+            <div className=" no-scrollbar flex flex-row sm:flex-col gap-4 max-w-xs sm:max-w-none max-h-none sm:max-h-[450px] overflow-auto">
               {product.images.map((imageURL, imageURLInedx) => (
                 <button
                   key={imageURLInedx}
@@ -37,7 +43,7 @@ export default function ProductSingle({ product }: { product: Product }) {
                   <img
                     key={imageURLInedx}
                     id={imageURL}
-                    className=" w-20 h-20"
+                    className=" w-20 h-20 max-w-none"
                     src={imageURL}
                     alt={imageURLInedx.toString()}
                   />
@@ -45,14 +51,14 @@ export default function ProductSingle({ product }: { product: Product }) {
               ))}
             </div>
             <img
-              className=" w-96 h-fit"
+              className=" w-96 h-fit hidden sm:block"
               key={product.id}
               src={selectedImage}
               alt={product.name}
             />
           </div>
 
-          <div className=" flex flex-col gap-14 min-w-[350px] max-w-lg mb-20">
+          <div className=" flex flex-col mx-auto gap-14 lg:min-w-[350px] max-w-xs lg:max-w-lg mb-20">
             <div className=" flex flex-col gap-3">
               <div className=" text-2xl font-bold">{product.name}</div>
               <div className=" text-xl ">{product.subtitle}</div>
